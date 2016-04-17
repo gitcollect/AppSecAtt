ad9824a59656ea83c1684644efdf9ae7ed35a900
MM Marinova <mm13354@it052568.users.bris.ac.uk>
2016-04-18 00:36:34 +0100

    Initial stage implemented

---
+++ fault/attack.h
@@ -0,0 +1,17 @@
+#ifndef __ATTACK_H
+#define __ATTACK_H
+
+#include  <cstdio>
+#include  <cstdlib>
+
+#include  <cstring>
+#include  <signal.h>
+#include  <unistd.h>
+#include  <fcntl.h>
+#include  <gmpxx.h>
+#include  <fstream>
+#include  <algorithm>
+#include  <openssl/aes.h>
+#include  <X11/Xlib.h>
+
+#endif
